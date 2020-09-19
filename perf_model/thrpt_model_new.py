2be6a5150328e6eaf3c1adec11e7f5556b3d5729
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-19 14:52:30 -0700

    Update thrpt_model_new.py

--- perf_model/thrpt_model_new.py
+++ perf_model/thrpt_model_new.py
@@ -232,6 +232,8 @@ def main():
               .format(args.split_train_name,
                       args.split_test_name,
                       args.split_rank_test_name))
+        train_df.rest_index(inplace=True)
+        test_df.reset_index(inplace=True)
         train_df.to_parquet(args.split_train_name)
         test_df.to_parquet(args.split_test_name)
         np.save(args.split_rank_test_name, test_rank_arr)
