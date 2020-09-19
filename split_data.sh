df075f0a5c653c80a6023369f7af2bdd1c91d2f0
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-19 16:19:43 -0700

    Update split_data.sh

--- split_data.sh
+++ split_data.sh
@@ -1,16 +1,14 @@
 set -ex
 
-mkdir split_tuning_dataset
 for fold in `ls tuning_dataset`;
 do
-    mkdir split_tuning_dataset/$fold
     for fname in `ls tuning_dataset/$fold/*.csv`;
     do
         python3 perf_model/thrpt_model_new.py  \
                 --split_test \
-                --dataset tuning_dataset/$fold/$fname \
-                --split_train_name split_tuning_dataset/$fold/$fname.train \
-                --split_test_name split_tuning_dataset/$fold/$fname.test \
-                --split_rank_test_name split_tuning_dataset/$fold/$fname.rank_test
+                --dataset $fname \
+                --split_train_name $fname.train \
+                --split_test_name $fname.test \
+                --split_rank_test_name $fname.rank_test
     done;
 done;
