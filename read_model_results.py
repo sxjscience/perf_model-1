259d563a85db0975712a619905e222448c88f4ed
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-22 13:12:40 -0700

    Update read_model_results.py

--- read_model_results.py
+++ read_model_results.py
@@ -4,7 +4,8 @@ import os
 import json
 
 parser = argparse.ArgumentParser(description='Read model results.')
-parser.add_argument('--model_name', type=str)
+parser.add_argument('--dir_path', type=str, default='model_results/cat_regression')
+parser.add_argument('--out_name', type=str, default='cat_regression')
 args = parser.parse_args()
 columns = ['name', 'rmse', 'mae', 'ndcg_all', 'ndcg_k3_all', 'mrr_all',
            'ndcg_valid', 'ndcg_k3_valid', 'mrr_valid']
@@ -18,4 +19,4 @@ for dir_name in os.listdir(args.dir_path):
                             dat['ndcg_all'], dat['ndcg_k3_all'], dat['mrr_all'],
                             dat['ndcg_valid'], dat['ndcg_k3_valid'], dat['mrr_valid']))
 df = pd.DataFrame(results, columns=columns)
-df.to_csv('f{args.model_name}.csv')
+df.to_csv('f{args.out_name}.csv')
