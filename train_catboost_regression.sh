14e4779ea68a9687bf2605d13142150107166c84
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-21 17:07:18 -0700

    update

--- train_catboost_regression.sh
+++ train_catboost_regression.sh
@@ -1,6 +1,6 @@
 set -ex
 
-mkdir -p model_results
+mkdir -p model_results/cat_regression
 for train_file in `ls split_tuning_dataset/*/*.train.pq`;
 do
   data_prefix=${train_file:0:-9}
@@ -8,5 +8,5 @@ do
   python3 -m perf_model.thrpt_model_new \
       --algo cat_regression \
       --data_prefix ${data_prefix} \
-      --out_dir model_results/${folder_prefix}/cat_regression
+      --out_dir model_results/cat_regression/${folder_prefix}
 done;
