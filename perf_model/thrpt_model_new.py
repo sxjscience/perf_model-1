164b3f76ac1231bcdc17ac57731ffc42410dd61f
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-23 01:48:41 -0700

    Update thrpt_model_new.py

--- perf_model/thrpt_model_new.py
+++ perf_model/thrpt_model_new.py
@@ -318,7 +318,7 @@ class CatRanker(CatRegressor):
             train_pool = catboost.Pool(data=step_features.reshape((-1, step_features.shape[-1])),
                                        label=step_thrpt.reshape((-1,)),
                                        group_id=step_groups.reshape((-1,)))
-            self.model.fit(train_pool)
+            self.model.fit(train_pool, eval_set=train_pool)
 
     def save(self, out_dir):
         self.model.save_model(os.path.join(out_dir, 'cat_ranking.cbm'))
