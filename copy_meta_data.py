7d93191cb47e7962239d8a25e924e363bb65e7da
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-23 11:41:01 -0700

    Update copy_meta_data.py

--- copy_meta_data.py
+++ copy_meta_data.py
@@ -4,10 +4,10 @@ import shutil
 
 parser = argparse.ArgumentParser(description='Read model results.')
 parser.add_argument('--dir_path', type=str, default='model_results/cat_ranking')
-parser.add_argument('--tuning_dataset', type=str, default='tuning_dataset')
+parser.add_argument('--tuning_data_path', type=str, default='tuning_dataset')
 args = parser.parse_args()
 
 for dir_name in os.listdir(args.dir_path):
     for model_name in os.listdir(os.path.join(args.dir_path, dir_name)):
-        meta_data_path = os.path.join(args.dir_path, dir_name, model_name + '.meta')
+        meta_data_path = os.path.join(args.tuning_data_path, dir_name, model_name + '.meta')
         shutil.copyfile(meta_data_path, os.path.join(args.dir_path, dir_name, 'feature.meta'))
