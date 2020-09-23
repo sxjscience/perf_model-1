4e3086886d686250d1b0e4c9fc32bd6402a5fe2e
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-23 12:44:26 -0700

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -3,11 +3,12 @@ import argparse
 import os
 import json
 import numpy as np
-from perf_model.thrpt_model_new import NNRanker, read_pd, get_feature_label, CatRegressor
+from perf_model.thrpt_model_new import NNRanker, read_pd, get_feature_label, CatRegressor,\
+    CatRanker
 
 parser = argparse.ArgumentParser(description='Read model results.')
 parser.add_argument('--dir_path', type=str, default='model_results/nn_5.0_200')
-parser.add_argument('--model_type', choices=['nn', 'cat_regression'])
+parser.add_argument('--model_type', choices=['nn', 'cat_regression', 'cat_ranking'])
 args = parser.parse_args()
 
 for dir_name in sorted(os.listdir(args.dir_path)):
@@ -16,6 +17,8 @@ for dir_name in sorted(os.listdir(args.dir_path)):
             model = NNRanker.load(os.path.join(args.dir_path, dir_name, exp_name))
         elif args.model_type == 'cat_regression':
             model = CatRegressor.load(os.path.join(args.dir_path, dir_name, exp_name))
+        elif args.model_type == 'cat_ranking':
+            model = CatRanker.load(os.path.join(args.dir_path, dir_name, exp_name))
         else:
             raise NotImplementedError
         data_prefix = os.path.join('split_tuning_dataset', dir_name, exp_name)
