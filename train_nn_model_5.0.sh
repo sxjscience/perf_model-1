1899f41d38b505b4892e866fe598977b71c17039
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-24 12:10:48 -0700

    Update train_nn_model_5.0.sh

---
+++ train_nn_model_5.0.sh
@@ -0,0 +1,18 @@
+set -ex
+
+rank_lambda=5.0
+iter_mult=200
+
+MODEL_DIR=model_results/nn_${rank_lambda}_${iter_mult}
+mkdir -p ${MODEL_DIR}
+for train_file in `ls split_tuning_dataset/*/*.train.pq`;
+do
+  data_prefix=${train_file:0:-9}
+  folder_prefix=${train_file:21:-9}
+  python3 -m perf_model.thrpt_model_new \
+      --algo nn \
+      --data_prefix ${data_prefix} \
+      --rank_lambda ${rank_lambda} \
+      --iter_mult ${iter_mult} \
+      --out_dir ${MODEL_DIR}/${folder_prefix}
+done;
