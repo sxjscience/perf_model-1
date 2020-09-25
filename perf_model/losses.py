651b596b2f47d606401e101450184b6779fe64e1
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-25 12:43:23 -0700

    Update losses.py

--- perf_model/losses.py
+++ perf_model/losses.py
@@ -126,6 +126,7 @@ def lambdaLoss(y_pred, y_true, eps=DEFAULT_EPS, k=None, sigma=1.,
               * torch.abs(G[:, :, None] - G[:, None, :])
     weights = torch.tril(weights)
     print('G=', G)
+    print('weights=', weights)
     # We are clamping the array entries to maintain correct backprop (log(0) and division by 0)
     scores_diffs = (y_pred_sorted[:, :, None] - y_pred_sorted[:, None, :]).clamp(min=-1e8, max=1e8)
     scores_diffs[torch.isnan(scores_diffs)] = 0.
