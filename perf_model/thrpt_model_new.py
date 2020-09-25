54f39042401e4e32b3d75193753e01aa4ed09616
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-25 11:11:58 -0700

    Update thrpt_model_new.py

--- perf_model/thrpt_model_new.py
+++ perf_model/thrpt_model_new.py
@@ -414,9 +414,9 @@ class NNRanker:
         for ranking_features, ranking_labels in dataloader:
             ranking_features = ranking_features.cuda()
             ranking_labels = ranking_labels.cuda()
-            original_ranking_labels = ranking_labels.reshape((batch_size, group_size))
-            ranking_labels = (ranking_labels - mean_val) / std_val
             ranking_labels = ranking_labels.reshape((batch_size, group_size))
+            original_ranking_labels = ranking_labels.copy()
+            ranking_labels = (ranking_labels - mean_val) / std_val
             optimizer.zero_grad()
             ranking_scores = self.net(ranking_features)
             ranking_scores = ranking_scores.reshape((batch_size, group_size))
