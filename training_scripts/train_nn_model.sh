2109bd461b08ab48295c7d6da28723206c2b3291
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-27 14:57:40 -0700

    Update train_nn_model.sh

--- training_scripts/train_nn_model.sh
+++ training_scripts/train_nn_model.sh
@@ -5,8 +5,8 @@ rank_lambda=$2
 iter_mult=$3
 task=$4
 
-data_prefix=split_tuning_dataset/$task
-MODEL_DIR=model_results/nn_${loss_type}_${rank_lambda}_${iter_mult}
+data_prefix=../split_tuning_dataset/$task
+MODEL_DIR=../model_results/nn_${loss_type}_${rank_lambda}_${iter_mult}
 mkdir -p ${MODEL_DIR}
 python3 -m perf_model.thrpt_model_new \
     --algo nn \
