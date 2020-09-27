cd3ce82f78bb4f686fc0e9ed6f7b42ff248e1007
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-27 14:31:13 -0700

    update

--- training_scripts/train_nn_model_1.0_lambda_rank_hinge.sh
+++ training_scripts/train_nn_model_1.0_lambda_rank_hinge.sh
@@ -1,41 +1,7 @@
 set -ex
 
 rank_lambda=1.0
-TASKS=(
-"gcv_graviton2_csv/conv2d_NCHWc.x86"
-"gcv_graviton2_csv/conv2d_nchw_spatial_pack.arm_cpu"
-"gcv_graviton2_csv/conv2d_nchw_winograd.arm_cpu"
-"gcv_graviton2_csv/dense_nopack.x86"
-"gcv_graviton2_csv/dense_pack.x86"
-"gcv_graviton2_csv/depthwise_conv2d_NCHWc.x86"
-"gcv_graviton2_csv/depthwise_conv2d_nchw.arm_cpu"
-
-"gcv_skylake_csv/conv2d_NCHWc.x86"
-"gcv_skylake_csv/dense_nopack.x86"
-"gcv_skylake_csv/dense_pack.x86"
-"gcv_skylake_csv/depthwise_conv2d_NCHWc.x86"
-
-#"gcv_t4_csv/conv2d_cudnn.cuda"
-"gcv_t4_csv/conv2d_nchw.cuda"
-"gcv_t4_csv/conv2d_nchw_winograd.cuda"
-"gcv_t4_csv/conv2d_transpose_nchw.cuda"
-#"gcv_t4_csv/dense_cublas.cuda"
-#"gcv_t4_csv/dense_large_batch.cuda"
-#"gcv_t4_csv/dense_small_batch.cuda"
-"gcv_t4_csv/dense_tensorcore.cuda"
-"gcv_t4_csv/depthwise_conv2d_nchw.cuda"
-
-#"gcv_v100_csv/conv2d_cudnn.cuda"
-"gcv_v100_csv/conv2d_nchw.cuda"
-"gcv_v100_csv/conv2d_nchw_winograd.cuda"
-"gcv_v100_csv/conv2d_transpose_nchw.cuda"
-#"gcv_v100_csv/dense_cublas.cuda"
-#"gcv_v100_csv/dense_large_batch.cuda"
-#"gcv_v100_csv/dense_small_batch.cuda"
-"gcv_v100_csv/dense_tensorcore.cuda"
-"gcv_v100_csv/depthwise_conv2d_nchw.cuda"
-)
-
+source ./tasks.sh
 
 N=8
 i=0
@@ -43,7 +9,7 @@ for iter_mult in 120;
 do
   for task in ${TASKS[@]};
   do
-    (( i=i%N ))
+    ((i=i%N))
     ((i++==0)) && wait
     echo $i
     ( data_prefix=split_tuning_dataset/$task
