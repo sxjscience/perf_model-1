e55b9260194b5a0bbd27b6245baa9c8d18b708b2
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-02 14:21:30 -0700

    Update evaluate_correlation_score.sh

--- evaluation_scripts/evaluate_correlation_score.sh
+++ evaluation_scripts/evaluate_correlation_score.sh
@@ -3,3 +3,15 @@ do
   python3 evaluate.py --eval_correlation --dir_path model_results/${model} --model_type cat_regression --correlation_out_name ${model}_corr
 done
 
+
+for model in nn_regression_split0.3_-1_1000_512_3_0.1_0 \
+             nn_regression_split0.3_-1_1000_512_3_0.1_1 \
+             nn_regression_split0.5_-1_1000_512_3_0.1_0 \
+             nn_regression_split0.5_-1_1000_512_3_0.1_1 \
+             nn_regression_split0.7_-1_1000_512_3_0.1_0 \
+             nn_regression_split0.7_-1_1000_512_3_0.1_1 \
+             nn_regression_split1_-1_1000_512_3_0.1_0 \
+             nn_regression_split1_-1_1000_512_3_0.1_1
+do
+  python3 evaluate.py --eval_correlation --dir_path model_results/${model} --model_type cat_regression --correlation_out_name ${model}_corr
+done
