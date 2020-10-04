046872f2059cfd59113cc160ef063a571274fe44
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-04 14:54:25 -0700

    Update split_data_op.sh

--- split_data_op.sh
+++ split_data_op.sh
@@ -5,17 +5,14 @@ mkdir -p ${OUT_DIR}
 for fold in `cat tasks.txt`;
 do
     mkdir -p ${OUT_DIR}/${fold}
-    for fname in `ls tuning_dataset/$fold/*.csv`;
-    do
-        prefix_name=${fname:0:-4}
-        python3 -m perf_model.thrpt_model_new  \
-                --split_test_op_level \
-                --dataset $fname \
-                --split_train_name ${prefix_name}.train.pq \
-                --split_test_name ${prefix_name}.test.pq \
-                --split_test_ratio 0.1 \
-                --seed 123
-        mv ${prefix_name}.train.pq ${OUT_DIR}/${fold}
-        mv ${prefix_name}.test.pq ${OUT_DIR}/${fold}
-    done;
+    dataset=tuning_dataset/${fold}
+    split_train_name=${OUT_DIR}/$fold.train.pq
+    split_test_name=${OUT_DIR}/$fold.test.pq
+    python3 -m perf_model.thrpt_model_new  \
+              --split_test_op_level \
+              --dataset ${datasets} \
+              --split_train_name ${split_train_name} \
+              --split_test_name ${split_test_name} \
+              --split_test_ratio 0.1 \
+              --seed 123
 done;
