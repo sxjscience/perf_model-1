1d98b606337f109ea8a973d34d65c3f7a6fdb0df
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-06 14:20:13 -0700

    Update get_average_scores.py

--- evaluation_scripts/get_average_scores.py
+++ evaluation_scripts/get_average_scores.py
@@ -9,7 +9,8 @@ parser.add_argument('--dir_path', type=str)
 parser.add_argument('--out_path', type=str)
 args = parser.parse_args()
 
-score_columns = ['spearman', 'ndcg-2', 'ndcg-5', 'ndcg-8']
+score_columns = ['spearman', 'ndcg-2', 'ndcg-5', 'ndcg-8',
+                 'ndcg-group-2', 'ndcg-group-5', 'ndcg-group-8']
 
 out = []
 index_l = []
