8511a727eba3f956c6d6ca9fc774d49307ce9d29
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-06 14:48:19 -0700

    Update evaluate_op_split_score.sh

--- evaluation_scripts/evaluate_op_split_score.sh
+++ evaluation_scripts/evaluate_op_split_score.sh
@@ -8,13 +8,13 @@ mkdir -p ${SAVE_DIR}
 #for model in cat_regression_split0.3 cat_regression_split0.5 cat_regression_split0.7 cat_regression_split1
 for model in cat_regression_op_5000_split1
 do
-  python3 evaluate.py --eval_correlation --dir_path ${MODEL_BASE_DIR}/${model} --model_type cat_regression --correlation_out_name ${SAVE_DIR}/${model}
+  python3 evaluate.py --eval_correlation --dir_path ${MODEL_BASE_DIR}/${model} --model_type cat_regression --use_op_split --correlation_out_name ${SAVE_DIR}/${model}
 done
 
 
 for model in cat_ranking_op_5000_split1
 do
-  python3 evaluate.py --eval_correlation --dir_path ${MODEL_BASE_DIR}/${model} --model_type cat_ranking --correlation_out_name ${SAVE_DIR}/${model}
+  python3 evaluate.py --eval_correlation --dir_path ${MODEL_BASE_DIR}/${model} --model_type cat_ranking --use_op_split --correlation_out_name ${SAVE_DIR}/${model}
 done
 
 
@@ -24,5 +24,5 @@ for model in nn_regression_op_split0.3_-1_1000_512_3_0.1_1 \
              nn_regression_op_split1_-1_1000_512_3_0.1_1 \
              nn_regression_op_split1_-1_1000_512_3_0.1_0
 do
-  python3 evaluate.py --eval_correlation --dir_path ${MODEL_BASE_DIR}/${model} --model_type nn --correlation_out_name ${SAVE_DIR}/${model}
+  python3 evaluate.py --eval_correlation --dir_path ${MODEL_BASE_DIR}/${model} --model_type nn --use_op_split --correlation_out_name ${SAVE_DIR}/${model}
 done
