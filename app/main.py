1325d1c8cf35a4b61143f70fcdf82c8546ae70a7
Xingjian Shi <xshiab@connect.ust.hk>
2020-12-05 23:10:28 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -239,6 +239,7 @@ def tune_kernels(tasks,
                 (idx, i.task.flop / np.mean(r.costs) / 1e9 if r.error_no == 0 else 0)
                 for idx, (i, r) in enumerate(zip(inputs, results))
             ], key=lambda x: x[1])
+            assert task.workload is not in best_results
             best_results[task.workload] = (best_idx, best_flops)
             sys.stderr.write(' | Best %.2f GFLOPS at Top %d | %.2fs\n' %
                              (best_flops, best_idx, time.time() - tic))
